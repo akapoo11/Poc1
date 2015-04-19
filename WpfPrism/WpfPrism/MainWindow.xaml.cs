81c773afa35e7653503d21480c188f9ec323d5fb
akapoo11 <amishkapoor@gmail.com>
2015-04-19 10:05:56 +0100

    test change

--- WpfPrism/WpfPrism/MainWindow.xaml.cs
+++ WpfPrism/WpfPrism/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+
 using System.Windows.Shapes;
 
 namespace WpfPrism
@@ -20,6 +21,7 @@ namespace WpfPrism
     /// </summary>
     public partial class MainWindow : Window
     {
+        // fo testing purpose
         public MainWindow()
         {
             InitializeComponent();
